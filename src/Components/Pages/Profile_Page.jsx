6703ae81f06b3638b23d18202e69c4f592878f53
alimdzhatdoev <alimdzhatdoev1@gmail.com>
2024-12-23 15:34:43 +0300

    Update

--- src/Components/Pages/Profile_Page.jsx
+++ src/Components/Pages/Profile_Page.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Container, Typography } from "@mui/material";
 import BottomNav from "./BottomNavigation";
 import { useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
+import { formatDate, getWeekNumber } from "../../data";
 
 const Profile_Page = ({ currentUser }) => {
     const navigate = useNavigate(); // Используем хук useNavigate
@@ -12,35 +13,105 @@ const Profile_Page = ({ currentUser }) => {
         navigate("/login"); // Правильное использование навигации
     };
 
+    const weekType = getWeekNumber();
     return (
-        <Box sx={{ pb: 7 }}>
-            <Typography variant="h5" fontWeight="bold" sx={{ mt: 3, textAlign: "center" }}>
-                Профиль
-            </Typography>
-            <Box sx={{ mt: 2, p: 2 }}>
-                <Typography variant="h6">Имя: {currentUser.name || "Гость"}</Typography>
-                <Typography>Группа: {currentUser.group}</Typography>
-                <Typography>Подгруппа: {currentUser.subgroup}</Typography>
-            </Box>
+        <Box
+            sx={{
+                height: "100vh",
+                p: "15px",
+                backgroundColor: "#f5f5f5",
+            }}
+        >
+            <Container sx={{ p: "0" }}>
+                <Typography
+                    component="p"
+                    sx={{
+                        fontSize: "13px",
+                        fontWeight: "600",
+                        color: "textSecondary",
+                    }}
+                >
+                    {formatDate()}
+                </Typography>
+                <Typography component="p" sx={{ fontSize: "24px", fontWeight: "700", margin: "15px 0 5px 0" }}>
+                    {currentUser.name || "Гость"}
+                </Typography>
+                <Typography component="p" sx={{ fontSize: "13px", fontWeight: "500", display: "flex" }}>
+                    Текущая неделя:{" "}
+                    <div style={{ color: "#81212D", fontWeight: "600", paddingLeft: "5px" }}>{weekType == 'numerator' ? "Числитель" : "Знаменатель"}</div>
+                </Typography>
 
-            <Button
-                variant="contained"
-                color="secondary"
-                fullWidth
-                sx={{
-                    height: "50px",
-                    fontSize: "16px",
-                    fontWeight: "bold",
-                    textTransform: "none",
-                    borderRadius: "8px",
-                }}
-                onClick={handleLogout}
-            >
-                Выйти
-            </Button>
+                <Box sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'space-between',
+                    height: 'calc(100vh - 183px)',
+                    mt: '30px',
+                }}>
+                    <Box sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        gap: '15px'
+                    }}>
+                        <Typography sx={{
+                            backgroundColor: '#fff',
+                            padding: '22px 20px',
+                            borderRadius: '10px',
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'space-between'
+                        }}>
+                            <Box sx={{ fontSize: '17px', fontWeight: '600' }}>Группа:</Box>
+                            <Box sx={{ fontSize: '15px', fontWeight: '500' }}>{currentUser.group}</Box>
+                        </Typography>
+
+                        <Typography sx={{
+                            backgroundColor: '#fff',
+                            padding: '22px 20px',
+                            borderRadius: '10px',
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'space-between'
+                        }}>
+                            <Box sx={{ fontSize: '17px', fontWeight: '600' }}>Подгруппа:</Box>
+                            <Box sx={{ fontSize: '15px', fontWeight: '500' }}>{currentUser.subgroup}</Box>
+                        </Typography>
+                    </Box>
+
+                    <Button
+                        variant="contained"
+                        color="secondary"
+                        fullWidth
+                        sx={{
+                            height: "50px",
+                            fontSize: "16px",
+                            fontWeight: "bold",
+                            textTransform: "none",
+                            borderRadius: "8px",
+                            boxShadow: 'none',
+                            mb: '15px',
+                            backgroundColor: '#81212D',
+                            color: '#fff'
+                        }}
+                        onClick={handleLogout}
+                    >
+                        Выйти
+                    </Button>
+
+                </Box>
+            </Container>
 
             <BottomNav active={2} />
         </Box>
+        // <Box sx={{ pb: 7 }}>
+        //     <Typography variant="h5" fontWeight="bold" sx={{ mt: 3, textAlign: "center" }}>
+        //         Профиль
+        //     </Typography>
+
+
+
+        //     <BottomNav active={2} />
+        // </Box>
     );
 };
 
